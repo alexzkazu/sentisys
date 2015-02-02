14df3428e6ed7e8e3fa8fbfcbd182c8c370f9621
alexzkazu <alex.ziebol@gmail.com>
2015-02-01 17:16:34 -0800

    added more tests

--- test/senti.spec.js
+++ test/senti.spec.js
@@ -1,25 +1,44 @@
 var expect = require("chai").expect;
 
-describe("strings", function() {
+var message = "";
+
+//set strings to variables
+describe("set strings", function() {
 
   beforeEach(function() {
 
    });
 
-	var positive = "";
-	var negative = "";
-  it("sets positive and negative to be strings", function() {
-    positive = "I am feeling so happy right now. Wonderful!"
-    negative = "I am feeling so sad right now. Depressed."
+  it("sets var to be strings", function() {
+    message = "I am feeling so happy right now. Wonderful! I sometimes feel sad though. Today is 2/1/15.";
 
-    expect(positive).to.equal("I am feeling so happy right now. Wonderful!");
-    expect(negative).to.equal("I am feeling so sad right now. Depressed.");
+    expect(message).to.equal("I am feeling so happy right now. Wonderful! I sometimes feel sad though. Today is 2/1/15.");
   });
 
-  it("sets positive and negative to be strings2", function() {
+});
 
-    expect(positive).to.equal("I am feeling so happy right now. Wonderful!");
-    expect(negative).to.equal("I am feeling so sad right now. Depressed.");
+//take the strings and break it down
+describe("break down the string", function() {
+
+	//lower case all characters
+  it("lowercase the characters", function() {
+    message = message.toLowerCase();
+
+    expect(message).to.equal("i am feeling so happy right now. wonderful! i sometimes feel sad though. today is 2/1/15.");
+  });
+
+  	//take out punctuations, numbers and save to an array
+  it("strip the punctuations, numbers, and symbols", function() {
+    message = message.replace(/[0-9'";:!@,.\/?\\-]/g, '');
+
+    expect(message).to.equal("i am feeling so happy right now wonderful i sometimes feel sad though today is ");
+  });
+
+  	//split the strings into array of items
+  it("splits the strings", function() {
+  	message = message.split(" ");
+
+    expect(message).to.deep.equal(["i","am","feeling","so","happy","right","now","wonderful","i","sometimes","feel","sad","though","today","is",""]);
   });
 
 });
